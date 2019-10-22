12573e546b34db42f0bfe2b7b7241a52a07833f4
Wiljan Vinke <wiljanvinke@gmail.com>
2019-10-22 13:10:02 +0200

    Added button to table

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 
 let requestTarget = "http://localhost:8082/";
+let tournamentDiv = document.getElementById("tournaments");
 
-function newUser(){
+function newUser() {
     let req = new XMLHttpRequest();
     req.open("POST", requestTarget + "participants", true);
     req.onload = onResponse;
@@ -9,11 +10,11 @@ function newUser(){
     req.send(JSON.stringify(getUserInput()));
 }
 
-function onResponse(){
+function onResponse() {
     alert(this.response);
 }
 
-function getUserInput(){
+function getUserInput() {
     let participant = {};
     participant.email = document.getElementById("email").value;
     participant.password = document.getElementById("password").value;
@@ -25,32 +26,38 @@ function getUserInput(){
     return participant;
 }
 
-function searchTournament(){
+function searchTournament() {
     var search = document.getElementById("searchbar").value;
-    console.log(search);
+    console.log("Search: " + search);
     let req = new XMLHttpRequest();
-    req.onreadystatechange = function() {
+    req.onreadystatechange = function () {
         //xhttp.responseType = "application/json";
         if (this.readyState == 4 && this.status == 200) {
             var tournaments = JSON.parse(req.responseText);
             console.log(tournaments);
             var table = document.createElement("TABLE");
-            for(var i = 0; i < tournaments.length; i++){
+            for (var i = 0; i < tournaments.length; i++) {
                 var row = table.insertRow(i);
                 row.insertCell(0).innerHTML = tournaments[i].name;
-                for(var j = 0; j < tournaments[i].categories.length; j++){
-                    var cell = row.insertCell(1);
+                var cell = row.insertCell(1);
+                for (var j = 0; j < tournaments[i].categories.length; j++) {                  
                     cell.innerHTML += tournaments[i].categories[j] + ", ";
                 }
                 row.insertCell(2).innerHTML = tournaments[i].startDate;
                 row.insertCell(3).innerHTML = tournaments[i].enrolDate;
-                row.insertCell(4);
-            
+                
+                let button = document.createElement('BUTTON');
+                button.innerHTML = "View";
+                row.insertCell(4).appendChild(button);
+
             }
-            document.getElementById("tournaments").appendChild(table); ;
-            
+            if(tournamentDiv.hasChildNodes()){
+                tournamentDiv.removeChild(tournamentDiv.children[0]);
             }
+            tournamentDiv.appendChild(table);
+
         }
-    req.open("GET", requestTarget + "tournaments/", true);
+    }
+    req.open("GET", requestTarget + "tournaments/?mode=contains&search=" + search, true);
     req.send();
 }
