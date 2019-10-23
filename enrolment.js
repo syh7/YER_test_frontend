1c8bbdd410722e0de11598c2d8ed62c9ddc90cb0
Wiljan Vinke <wiljanvinke@gmail.com>
2019-10-23 16:08:32 +0200

    Added single row to enrolment form

--- enrolment.js
+++ enrolment.js
@@ -2,10 +2,13 @@ let requestTarget = "http://localhost:8082/";
 
 
 load()
-
 function load() {
     let id = new URL(location.href).searchParams.get('id')
     let name = new URL(location.href).searchParams.get('t')
     console.log("Tournament.id: " + id);
-    document.getElementById("name").innerHTML = name;
+    document.getElementById("name").innerHTML = name;    
+}
+
+function submit(){
+    
 }
