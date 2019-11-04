2898a9cd0c7c61edd3e4487bef0298d3c9bb2cd3
Student <sjoerd0707@live.nl>
2019-11-04 16:16:48 +0100

    fix buttons

--- index/index.js
+++ index/index.js
@@ -3,4 +3,5 @@ function signIn() {
     let userName = document.getElementById("userName").value;
     let password = document.getElementById("password").value;
     console.log(userName);
+    return false;
 }
