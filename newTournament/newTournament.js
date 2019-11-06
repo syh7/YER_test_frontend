ddea05c6ff42177b5133ce16a2dcfcce9030c057
Student <sjoerd0707@live.nl>
2019-11-06 09:44:00 +0100

    Add functionality to navbar

--- newTournament/newTournament.js
+++ newTournament/newTournament.js
@@ -1,3 +1,20 @@
-function goBack(){
+function goHome() {
+    let admin = JSON.parse(localStorage.getItem("admin"));
+    window.location.href = "../admin/admin.html?id=" + admin.id;
+}
+
+function goBack() {
     window.history.back();
+}
+
+function editUser() {
+    let admin = JSON.parse(localStorage.getItem("admin"));
+    window.location.href = "../newadmin/newadmin.html?id=" + admin.id;
+}
+
+function logout() {
+    if (confirm("Are you sure you want to logout?")) {
+        alert("You have logged out.");
+        window.location.href = "../index/index.html";
+    }
 }
