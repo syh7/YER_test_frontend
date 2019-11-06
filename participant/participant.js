446cd9bbc2e3bcc34665c4f0761addde513f5433
Student <sjoerd0707@live.nl>
2019-11-06 12:57:19 +0100

    Remove unnecessary comments

--- participant/participant.js
+++ participant/participant.js
@@ -10,13 +10,11 @@ load();
 */
 function searchTournament() {
     var search = document.getElementById("searchbar").value;
-    console.log("Search: " + search);
     let req = new XMLHttpRequest();
     req.onreadystatechange = function () {
         //Get tournaments from backend
         if (this.readyState == 4 && this.status == 200) {
             tournaments = JSON.parse(req.responseText);
-            console.log("Tournaments: " + tournaments);
             if (searchTournamentContainer.hasChildNodes()) {
                 searchTournamentContainer.removeChild(searchTournamentContainer.children[0]);
             }
@@ -78,7 +76,6 @@ function makeTournamentTable(tournaments, tourDiv) {
     row.insertCell(4).innerHTML = "<b>Enrol Date</b> &#x2193;";
 
     for(let i = 0; i < row.cells.length; i++){
-        console.log("test " + i);
         row.cells[i].onclick = function(){
             sortTable(table, i);
         }
@@ -118,26 +115,16 @@ function sortTable(table, n) {
         // Start by saying: no switching is done:
         switching = false;
         rows = table.rows;
-        /* Loop through all table rows (except the
-        first, which contains table headers): */
         for (i = 1; i < (rows.length - 1); i++) {
-            // Start by saying there should be no switching:
             shouldSwitch = false;
-            /* Get the two elements you want to compare,
-            one from current row and one from the next: */
             x = rows[i].getElementsByTagName("TD")[n];
             y = rows[i + 1].getElementsByTagName("TD")[n];
-            /* Check if the two rows should switch place,
-            based on the direction, asc or desc: */
             if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-                // If so, mark as a switch and break the loop:
                 shouldSwitch = true;
                 break;
             }
         }
         if (shouldSwitch) {
-            /* If a switch has been marked, make the switch
-            and mark that a switch has been done: */
             rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
             switching = true;
         }
@@ -149,7 +136,7 @@ function requestGameStatistics() {
     req.open("GET", serverIP + "participants/" + participant.id + "/results", true);
     req.onload = function () {
         let parsedResponse = JSON.parse(this.response);
-        if (parsedResponse[0] > 0) { //er is data
+        if (parsedResponse[0] > 0) { 
             makeGamesChart(parseStatistics(parsedResponse));
         }
     }
@@ -194,7 +181,6 @@ function editUser() {
 
 //Redirect to tournament page
 function openTournament(tournamentId) {
-    console.log("In function, id=: " + tournamentId);
     window.location.href = '../tournament/tournament.html?id=' + tournamentId;
 }
 
